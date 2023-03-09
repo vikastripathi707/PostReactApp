6dd823156b7227445cb647e78c45a61c865d8cbd
VIKAS TRIPATHI <86002620+vikastripathi707@users.noreply.github.com>
2023-03-09 20:13:49 +0530

    Update Home.jsx

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -55,7 +55,7 @@ function Home() {
       <h5 className="card-title">Card title</h5>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <div className="d-grid">
-        <a href="#" className="btn btn-warning">read more</a>
+        <a href="#" className="btn btn-warning">Know More</a>
       </div>
     </div>
     </div>
@@ -70,7 +70,7 @@ function Home() {
       <h5 className="card-title">Card title</h5>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <div className="d-grid">
-        <a href="#" className="btn btn-warning">read more</a>
+        <a href="#" className="btn btn-warning">Know More</a>
       </div>
     </div>
     </div>
@@ -85,7 +85,7 @@ function Home() {
       <h5 className="card-title">Card title</h5>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <div className="d-grid">
-        <a href="#" className="btn btn-warning">read more</a>
+        <a href="#" className="btn btn-warning">Know More</a>
       </div>
     </div>
     </div>
@@ -107,7 +107,7 @@ function Home() {
       <h5 className="card-title">Card title</h5>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <div className="d-grid">
-        <a href="#" className="btn btn-warning">read more</a>
+        <a href="#" className="btn btn-warning">Know More</a>
       </div>
     </div>
     </div>
@@ -122,7 +122,7 @@ function Home() {
       <h5 className="card-title">Card title</h5>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <div className="d-grid">
-        <a href="#" className="btn btn-warning">read more</a>
+        <a href="#" className="btn btn-warning">Know More</a>
       </div>
     </div>
     </div>
@@ -137,7 +137,7 @@ function Home() {
       <h5 className="card-title">Card title</h5>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <div className="d-grid">
-        <a href="#" className="btn btn-warning">read more</a>
+        <a href="#" className="btn btn-warning">Know More</a>
       </div>
     </div>
     </div>
