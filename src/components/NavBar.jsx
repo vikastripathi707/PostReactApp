2c571361004ed82101f290bb8a5a686e9455e09d
VIKAS TRIPATHI <86002620+vikastripathi707@users.noreply.github.com>
2023-03-09 17:09:08 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -66,7 +66,7 @@ const NavBar = () => {
     <div>
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient">
   <div className="container-fluid">
-    <NavLink className="navbar-brand text-uppercase" to={"/"}>Galleria</NavLink>
+    <NavLink className="navbar-brand text-uppercase" to={"/"}>My Post App</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
