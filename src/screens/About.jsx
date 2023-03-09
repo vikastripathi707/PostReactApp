6ba1dc0d67ac3ee7fbf8c8d10bd53bcfff5d8c9e
VIKAS TRIPATHI <86002620+vikastripathi707@users.noreply.github.com>
2023-03-09 17:26:10 +0530

    Update About.jsx

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -5,11 +5,16 @@ function About() {
     <div className='container'>
     <h2 className='pt-4 text-center text-uppercase' >About us</h2>
     <div className=' text-muted  shadow-sm rounded p-3 lh-2'>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at cumque dolor ullam animi, voluptatum corrupti quod, explicabo nisi distinctio eaque ut numquam voluptas esse eveniet, dolorum laborum architecto molestiae
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at cumque dolor ullam animi, voluptatum corrupti quod, explicabo nisi distinctio eaque ut numquam voluptas esse eveniet, dolorum laborum architecto molestiae.</p>
     
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at cumque dolor ullam animi, voluptatum corrupti quod, explicabo nisi distinctio eaque ut numquam voluptas esse eveniet, dolorum laborum architecto molestiae
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at cumque dolor ullam animi, voluptatum corrupti quod, explicabo nisi distinctio eaque ut numquam voluptas esse eveniet, dolorum laborum architecto molestiae. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde exercitationem, velit voluptatum autem voluptate commodi optio facilis esse, quod est possimus nam facere necessitatibus, non voluptatem reprehenderit ut praesentium fuga? </p>
+   Welcome to our project!
+
+We are a team of passionate individuals who came together with a common goal - to create something that can make a positive impact in the world. Our project was born out of the desire to solve a real-world problem that we believe has the potential to change lives.
+
+Our diverse team comprises of experts from different backgrounds and skill sets. From designers and developers to researchers and marketers, we bring a wealth of experience and knowledge to the table. This allows us to approach challenges from different angles and come up with innovative solutions.
+
+<br /> At the heart of our project is a commitment to quality and excellence. We believe that the key to success lies in delivering a product that not only meets but exceeds the expectations of our users. This is why we are constantly refining and improving our work, always striving to be better.
+
+But what sets us apart is not just our technical expertise or commitment to quality - it's our passion for what we do. We truly believe that our project can make a difference in people's lives, and we are excited to share it with the world.</p>
 
     </div>
     </div>
